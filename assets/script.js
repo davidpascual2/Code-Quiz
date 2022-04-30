3974adced174d81cb1e5d791285e61173309c0f1
dave <davidpascual200@gmail.com>
2022-04-29 19:47:28 -0700

    added JS commands

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,33 @@
-// var time = document.querySelector(".time");
-// var secondsLeft = 20;
 
-// function setTime() {
-    
-// }
+// var secondsLeft = 20;
 
 var startButton = document.querySelector(".container"); //.start-button
 var questionContainerElement = document.querySelector("#question-container");
-var answerButtonsElement = document.querySelector(".qbutton") //why is this needed?
-let currentQuestion = 0
+var answerButtonsEl = document.querySelector(".qbutton") //why is this needed?
+let currentQuestion = 0 //set to 0 because we want to start on first Question of questions array
+//var timerEl = document.querySelector(".timer");
 
-var questions = [
+var questionList = [
     {
-        questiontext: "What does HTML mean?",
+        questionText: "what is 2 + 2?",
+        choices: ["4", "6", "44", "12"], 
+        answer: "4"
+    },
+
+    {
+        questionText: "what color is the sky",
+        choices: ["blue", "red,", "green", "purple"],
+        answer: "blue"
+    },
+
+    {
+        questionText: "What does HTML mean?",
         choices: ["hello", "hi there,", "exmaple", "okay"],
         answer: "hello"
     },
 
     {
-        questiontext: "What does HTML mean?",
-        choices: ["hello", "hi there,", "exmaple", "okay"],
-        answer: "hello"
-    },
-
-    {
-        questiontext: "What does HTML mean?",
-        choices: ["hello", "hi there,", "exmaple", "okay"],
-        answer: "hello"
-    },
-
-    {
-        questiontext: "What does HTML mean?",
+        questionText: "What does HTML mean?",
         choices: ["hello", "hi there,", "exmaple", "okay"],
         answer: "hello"
     },
@@ -42,40 +39,50 @@ var questions = [
 startButton.addEventListener("click", startGame) //prevent default? where does it go?
 
 
-
 //start the game
 function startGame(){
-    console.log("Hey you clicked the start button!");
+
+    score = 0
+    // questionList = getQuestionsList()
+    // timeLeft = 0
+    // console.log("Hey you clicked the start button!");
     startButton.style.display="none";
     //shuffledQuestions = questions.sort(() => Math.random() -.5);
     // currentQuestionIndex = 0 //what does this do (starting on first question from questions array)
     questionContainerElement.classList.remove("hide");
     
-    showQuesion();
-
+    showQuesion(); //create function below. cannot call if there is no function
 }
 
 
 //set next question fucntion once next is pressed
 function showQuesion(){
     var questionTextEl = document.querySelector(".question-text");
-    questionTextEl.innerText = questions[currentQuestion].questionText;
+    questionTextEl.innerText = questionList[currentQuestion].questionText;
 
-    var choiceButton0 = document.querySelector("#btn0");
-    choiceButton0.innerText = questions[currentQuestion].choices[0];
+    // manually call buttons below //
 
+    // var choiceButton0 = document.querySelector("#btn0");
+    // choiceButton0.innerText = questions[currentQuestion].choices[0];
 
+    // var choiceButton1 = document.querySelector("#btn1");
+    // choiceButton1.innerText = questions[currentQuestion].choices[1];
+
+    // var choiceButton2 = document.querySelector("#btn2");
+    // choiceButton2.innerText = questions[currentQuestion].choices[2];
+
+    // var choiceButton3 = document.querySelector("#btn3");
+    // choiceButton3.innerText = questions[currentQuestion].choices[3];
     
-    
-    
-    
+    for(i=0; i < 4; i++){
+        console.log(i) // this shows that the loop registered answer buttons
+        var buttonEl = document.querySelector("#btn" + i)
+        console.log("btn" + i); // this prints
+        buttonEl.innerText = questionList[currentQuestion].choices[i]
+    }
+
 } 
-    // for(i=0; i < 4; i++){
-    //     console.log(i)
-    //     var button = document.querySelector("#btn" + i)
-    //     console.log("btn" + i);
-    //     button.innerText = questions [currentQuestion].choices[i]
-    // }
+    
 
 
 // function showQuestion(question){
