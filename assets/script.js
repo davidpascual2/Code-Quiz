b8d68f05399d606d1e9ed1be143c414f44c2a699
dave <davidpascual200@gmail.com>
2022-04-30 22:22:17 -0700

    added user choice check

--- assets/script.js
+++ assets/script.js
@@ -2,71 +2,90 @@
 // var secondsLeft = 20;
 
 var startButton = document.querySelector(".container"); //.start-button
-var questionContainerElement = document.querySelector("#question-container");
+var questionContainerEl = document.querySelector("#question-container");
 var answerButtonsEl = document.querySelector(".qbutton") //why is this needed?
 let currentQuestion = 0 //set to 0 because we want to start on first Question of questions array
+let curScore = 0;
 //var timerEl = document.querySelector(".timer");
 
 var questionList = [
     {
-        questionText: "what is 2 + 2?",
-        choices: ["4", "6", "44", "12"], 
-        answer: "4"
+        questionText: "javaScript file has an extension of",
+        choices: [".java", ".js", ".javascript", ".xml"], 
+        answer: ".js"
     },
 
     {
-        questionText: "what color is the sky",
-        choices: ["blue", "red", "green", "purple"],
-        answer: "blue"
+        questionText: "An array Index starts with?",
+        choices: ["-1", "0", "1", "2"],
+        answer: "0"
+    },
+
+    {
+        questionText: "In what year was javascript created?",
+        choices: ["1970", "1990", "1995", "1981"],
+        answer: "1995"
     },
 
     {
         questionText: "What does HTML mean?",
-        choices: ["hello", "hi there", "exmaple", "okay"],
-        answer: "hello"
-    },
-
-    {
-        questionText: "What does HTML mean?",
-        choices: ["hello", "hi there", "exmaple", "okay"],
-        answer: "hello"
+        choices: ["Hyper Text Markup Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Trainer Marking Language"],
+        answer: "Hyper Text Markup Language"
     },
 ]
-// var questionElement = document.querySelector("#question"); //why is this needed?
-
- //index? , shuffledQuestions, MUST be "let"??
 
 startButton.addEventListener("click", startGame) //prevent default? where does it go?
 
-
+// function gameOver(){
+//     showQuestion("gameover");
+// }
 //start the game
 function startGame(){
 
-    score = 0
+    curScore = 0
     startButton.style.display="none";
-    questionContainerElement.classList.remove("hide");
+    questionContainerEl.classList.remove("hide");
     // questionList = getQuestionsList()
     // timeLeft = 0
-    // console.log("Hey you clicked the start button!");
     //shuffledQuestions = questions.sort(() => Math.random() -.5);
     // currentQuestionIndex = 0 //what does this do (starting on first question from questions array)
-    
     
     showQuestion(); //create function below. cannot call if there is no function
 }
 
 
 //set next question fucntion once next is pressed
-
-function quizIndex(){ 
-    // check if user right or wrong 
-    currentQuestion = currentQuestion + 1;
-    showQuestion()
-}
+// function selectAnswer(){ 
+    function selectAnswer(event){ 
+        // check if user right or wrong 
+        if(event.target.innerText === questionList[currentQuestion].answer){
+            console.log("correct");
+            curScore += 1;
+            console.log(curScore);
+        }else{
+            console.log("wrong");
+            curScore = score - 1
+        }
+    
+        currentQuestion = currentQuestion + 1;
+        showQuestion()
+    //     currentQuestion = currentQuestion + 1; //transitions to next question when button clicked
+    // showQuestion()
+    }
+    
+    // check if user right or wrong
+    
+    // currentQuestion = currentQuestion + 1; //transitions to next question when button clicked
+    // showQuestion()
+// }
 
 function showQuestion(){
-    var questionTextEl = document.querySelector(".question-text");
-    questionTextEl.innerText = questionList[currentQuestion].questionText;
+    // if (questionList === 0) {
+    //     gameOver()
+    // } else {
+
+    var questionTextEl = document.querySelector(".question-text"); //defines questionTextEL var
+    questionTextEl.innerText = questionList[currentQuestion].questionText; // displays questions 
 
     // manually call buttons below //
 
@@ -82,25 +101,33 @@ function showQuestion(){
     // var choiceButton3 = document.querySelector("#btn3");
     // choiceButton3.innerText = questions[currentQuestion].choices[3];
     
+    
+    //displays button options with event listener. 
     for(i=0; i < 4; i++){
         console.log(i) // this shows that the loop registered answer buttons
         var buttonEl = document.querySelector("#btn" + i)
         console.log("btn" + i); // this prints
         buttonEl.innerText = questionList[currentQuestion].choices[i]
-        buttonEl.addEventListener("click", quizIndex)
+        buttonEl.addEventListener("click", selectAnswer)
     
     }
-
+    // }
 } 
     
-
-
-// function showQuestion(question){
-//     questionElement.innerText = question.question
-// }
-
 //create a function when answer is selected
-// function selectAnswer(){
 
+
+// for loop for user check
+// function selectAnswer(e) {
+//     console.log("you clicked an answer!");
+//     const userChoice = e.target;
+//     const correctChoice = userChoice.questionList.correct;
+//     if (answer) {
+//         console.log("yes")
+//         // score += 10;
+//     } else {
+//         console.log("no")
+//     // score -= 10;
+//     }
 // }
 
