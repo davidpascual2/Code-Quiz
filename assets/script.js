cd72940904e33621a7d177f441256ced70f0fc9f
dave <davidpascual200@gmail.com>
2022-05-02 01:20:56 -0700

    added timer and gameover function

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,8 @@ var questionContainerEl = document.querySelector("#question-container");
 var answerButtonsEl = document.querySelector(".qbutton") //why is this needed?
 let currentQuestion = 0 //set to 0 because we want to start on first Question of questions array
 let curScore = 0;
-// let QuizEnd = document.queryselector(".gameover"); ???
-//var timerEl = document.querySelector(".timer");
+let QuizEndEl = document.querySelector("#results-container"); 
+var timerEl = document.querySelector(".timer");
 
 var questionList = [
     {
@@ -34,19 +34,44 @@ var questionList = [
     },
 ]
 
-startButton.addEventListener("click", startGame) //prevent default? where does it go?
+startButton.addEventListener("click", startGame); //prevent default? where does it go?
 
-// function gameOver(){
-//     showQuestion("gameover");
-// }
+function gameOver(){
+    questionContainerEl.style.display="none";
+    QuizEndEl.classList.remove("hide");
+    document.querySelector(".finalscore").textContent = "you scored " + curScore + " points";
+}
 
-// function TimeLeft() {
-//     timerEl.innerText = 
-// }
+function countdown() {
+    var timeLeft = 5;
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = '';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        // Call the `displayMessage()` function
+        gameOver();
+      }
+    }, 1000);
+  }
+
 //start the game
 function startGame(){
 
-    curScore = 0
+    curScore = 0;
     startButton.style.display="none";
     questionContainerEl.classList.remove("hide");
     // questionList = getQuestionsList()
@@ -55,45 +80,54 @@ function startGame(){
     // currentQuestionIndex = 0 //what does this do (starting on first question from questions array)
     
     showQuestion(); //create function below. cannot call if there is no function
+    countdown()
 }
 
-
-//set next question fucntion once next is pressed
-    function selectAnswer(event){ 
-        // check if user right or wrong 
-        if(event.target.innerText === questionList[currentQuestion].answer){
-            console.log("correct");
-            curScore += 1;
-            // event.target.classList.add(".green")
-            console.log(curScore);
-        }else{
-            console.log("wrong");
-            // event.target.classList.add(".red")
-            curScore = score - 1
-        }
-        // setTimeout(showQuestion, 1000);
-    
-        currentQuestion = currentQuestion + 1; //transitions to next question when button clicked
-        if (currentQuestion < questionList.length) {
-            showQuestion()
-        } else {
-            console.log("Quiz Completed!")
-        }
-        
-    }
-    
-    // check if user right or wrong
-    
-
-    function showQuestion(){
-    // if (questionList === 0) {
-    //     gameOver()
-    // } else {
+function showQuestion(){
 
     var questionTextEl = document.querySelector(".question-text"); //defines questionTextEL var
     questionTextEl.innerText = questionList[currentQuestion].questionText; // displays questions 
 
-    // manually call buttons below //
+    //displays button options with event listener. 
+    for(i=0; i < 4; i++){
+        console.log(i) // this shows that the loop registered answer buttons
+        var buttonEl = document.querySelector("#btn" + i)
+        console.log("btn" + i); // this prints
+        buttonEl.innerText = questionList[currentQuestion].choices[i]
+        buttonEl.addEventListener("click", selectAnswer)
+
+    }
+} 
+
+//set next question fucntion once next is pressed
+function selectAnswer(event){ 
+    // check if user right or wrong 
+    if(event.target.innerText === questionList[currentQuestion].answer){
+        console.log("correct");
+        curScore += 1;
+        // event.target.classList.add(".green")
+        console.log(curScore);
+    }else{
+        console.log("wrong");
+        // event.target.classList.add(".red")
+        curScore = curScore - 1
+    }
+        // setTimeout(showQuestion, 1000);
+
+    currentQuestion = currentQuestion + 1; //transitions to next question when button clicked
+    if (currentQuestion < questionList.length) {
+        showQuestion()
+    } else {
+        console.log("Quiz Completed!");
+        gameOver();
+    }
+    
+}
+    
+    // check if user right or wrong
+
+
+// how to manually call buttons //
 
     // var choiceButton0 = document.querySelector("#btn0");
     // choiceButton0.innerText = questions[currentQuestion].choices[0];
@@ -106,34 +140,6 @@ function startGame(){
 
     // var choiceButton3 = document.querySelector("#btn3");
     // choiceButton3.innerText = questions[currentQuestion].choices[3];
-    
-    
-    //displays button options with event listener. 
-    for(i=0; i < 4; i++){
-        console.log(i) // this shows that the loop registered answer buttons
-        var buttonEl = document.querySelector("#btn" + i)
-        console.log("btn" + i); // this prints
-        buttonEl.innerText = questionList[currentQuestion].choices[i]
-        buttonEl.addEventListener("click", selectAnswer)
-    
-    }
-    // }
-} 
-    
-//create a function when answer is selected
 
 
-// for loop for user check
-// function selectAnswer(e) {
-//     console.log("you clicked an answer!");
-//     const userChoice = e.target;
-//     const correctChoice = userChoice.questionList.correct;
-//     if (answer) {
-//         console.log("yes")
-//         // score += 10;
-//     } else {
-//         console.log("no")
-//     // score -= 10;
-//     }
-// }
 
